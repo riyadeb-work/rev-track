112dba7e62f2f21833b42ac0326d09531cb87124
Riya Deb <riyadeb.work@gmail.com>
2026-04-09 01:08:31 +0530

    Added Config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Keep this file out of version control (Git)
+export const firebaseConfig = {
+    apiKey: "AIzaSyDUq34CCdpW_M-i5j4HkfpnfvZaCJyX5wQ",
+    authDomain: "meghive-tools.firebaseapp.com",
+    projectId: "meghive-tools",
+    storageBucket: "meghive-tools.firebasestorage.app",
+    messagingSenderId: "174238860069",
+    appId: "1:174238860069:web:3b3f571ef4eab8e302d0cc"
+};
